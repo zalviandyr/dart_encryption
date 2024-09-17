d844eb19e2a9991450baa649560ab4f1f0fce246
zalviandyr <zukronalviandy@gmail.com>
2024-09-17 09:17:28 +0700

    init

---
+++ bin/dart_encryption.dart
@@ -0,0 +1,5 @@
+import 'package:dart_encryption/dart_encryption.dart' as dart_encryption;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_encryption.calculate()}!');
+}
---
+++ test/dart_encryption_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_encryption/dart_encryption.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
