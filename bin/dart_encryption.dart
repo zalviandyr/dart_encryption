77db2a43c8e7d4fda2f73081c075a17249cb868a
zalviandyr <zukronalviandy@gmail.com>
2024-09-18 11:00:01 +0700

    feat: aes

--- bin/dart_encryption.dart
+++ bin/dart_encryption.dart
@@ -1,5 +1,15 @@
 import 'package:dart_encryption/dart_encryption.dart' as dart_encryption;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_encryption.calculate()}!');
+  Map<String, dynamic> json = {
+    'email': 'zukronalviandy@gmail.com',
+    'password': 'asdf1234'
+  };
+  // change to your key
+  String strKey = 'HayoooApaHayooo~';
+
+  final encrypt = dart_encryption.encrypt(json, strKey);
+  final decrypt = dart_encryption.decrypt(encrypt, strKey);
+
+  print(decrypt);
 }
--- lib/dart_encryption.dart
+++ lib/dart_encryption.dart
@@ -1,3 +1,34 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:encrypt/encrypt.dart';
+
+String encrypt(Map<String, dynamic> json, String strKey) {
+  String text = jsonEncode(json);
+  IV iv = IV.fromLength(16);
+  Key key = Key.fromUtf8(strKey);
+
+  Encrypter encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+  Encrypted encrypted = encrypter.encrypt(text, iv: iv);
+
+  List<int> cipher = iv.bytes + encrypted.bytes;
+  return base64Encode(cipher);
+}
+
+Map<String, dynamic> decrypt(String decryptBase64, String strKey) {
+  Uint8List decodedData = base64Decode(decryptBase64);
+
+  // extract iv and ciphertext
+  int ivSize = 16;
+  Uint8List cipher = decodedData.sublist(ivSize);
+  IV iv = IV(decodedData.sublist(0, ivSize));
+  Encrypted encrypted = Encrypted(cipher);
+
+  // encryption key
+  Key key = Key.fromUtf8(strKey);
+
+  Encrypter encrypter = Encrypter(AES(key, mode: AESMode.cbc));
+  String decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+  return jsonDecode(decrypted);
 }
